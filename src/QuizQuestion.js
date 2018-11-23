2857c411e21b0d87cb0beda03443c3135461ff41
Xiao Wang <xiawang@microsoft.com>
2018-11-23 19:28:34 +0100

    add alert message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+    constructor(props){
+        super(props)
+        this.state={incorrectAnswer:false}
+    }
+
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer)
         {
             this.props.showNextQuestionHandler()
+            this.setState(state=>{
+                return { incorrectAnswer:false}
+            })
+        }else{
+            this.setState((state)=>{
+                return {incorrectAnswer:true}
+            })
         }
     }
     render(props) {
@@ -25,6 +37,14 @@ class QuizQuestion extends Component {
                         )}    
                     </ul>
                 </section>
+                {
+                 (this.state.incorrectAnswer)?
+                        <p className='error'>
+                        Sorry, this is not right
+                        </p>
+                    :
+                        null
+                }
             </main>
             );
     }
